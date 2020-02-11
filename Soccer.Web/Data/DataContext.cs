01501079945f248d4912ced4543b9ea24da05989
Deymer <deymer.ap@gmail.com>
2020-02-10 22:06:33 -0500

    AddTeamEntity

---
+++ Soccer.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Soccer.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Entities;
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+       public DbSet<TeamEntity> Teams { get; set; }
+    }
+}
